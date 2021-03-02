441e5d758570b25a0be14e0c38b32d85a0c2c7a3
ccoradi <ccoradi@njcu.edu>
2021-03-02 00:46:38 +0000

    Finished homework 12

---
+++ CaueCoradi/noCTRLC.sh
@@ -0,0 +1,29 @@
+#!/bin/bash 
+
+ctrlc_count=0
+
+function no_ctrlc()
+{
+    let ctrlc_count++
+    echo
+    if [[ $ctrlc_count == 1 ]]; then
+        echo "Stop that."
+    elif [[ $ctrlc_count == 2 ]]; then
+        echo "Stop that."
+    elif [[ $ctrlc_count == 3 ]]; then
+        echo "Stop that."
+    elif [[ $ctrlc_count == 4 ]]; then
+        echo "Once more and I quit."
+    else
+        echo "That's it.  I quit."
+        exit
+    fi
+}
+
+trap no_ctrlc SIGINT
+
+while true
+do
+    echo Sleeping
+    sleep 2
+done
